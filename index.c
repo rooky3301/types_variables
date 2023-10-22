e58d324893315db502e2d559e448d656e73bd3d1
ScrviyyDev <91909966+ScrviyyDev@users.noreply.github.com>
2023-10-21 21:31:39 -0300

    add main function on index

--- index.c
+++ index.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-  int x;
-  x = 1000000;
 
-  printf("%d", x);
+  int idade;
+  float salario, altura;
+  char genero;
+  char nome[50];
+
+  idade = 20;
+  salario = 5800.5;
+  altura = 1.63;
+  genero = 'F';
+  strcpy(nome, "Maria Silva");
+
+  printf("IDADE = %d\n", idade);
+  printf("SALARIO = %.2lf\n", salario);
+  printf("ALTURA = %.2lf\n", altura);
+  printf("GENERO = %c\n", genero);
+  printf("NOME = %s\n", nome);
 
   return 0;
 }
